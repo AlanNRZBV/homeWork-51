94685e4548f6a69b9a6ff3949ad799e62653541b
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2023-10-18 16:43:11 +0600

    feat: implemented random numbers generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,46 @@
-import { useState } from 'react'
-import './App.css'
-import LotteryBall from "../LotteryBall/LotteryBall.tsx";
+import { useState } from 'react';
+import './App.css';
+import LotteryBall from '../LotteryBall/LotteryBall.tsx';
 
 function App() {
   const [lotteryBall, setLotteryBall] = useState([
-    {number: 5},
-    {number: 11},
-    {number: 16},
-    {number: 23},
-    {number: 32}
-  ])
+    { number: 5 },
+    { number: 11 },
+    { number: 16 },
+    { number: 23 },
+    { number: 32 },
+  ]);
 
-  const generateNumbers = ()=>{
-    console.log('Test')
-    const numbersStorage:[] = [];
+  const generateNumbers = () => {
+    const numbersStorage: [number] = [];
     while (numbersStorage.length < 5) {
-      const randomNumber:number = Math.floor(Math.random() * 28) + 5;
-
+      const randomNumber: number = Math.floor(Math.random() * 28) + 5;
       if (numbersStorage.indexOf(randomNumber) === -1) {
         numbersStorage.push(randomNumber);
       }
     }
-
-    console.log(numbersStorage)
-    // setLotteryBall(
-    //     [
-    //
-    //     ]
-    // )
-  }
+    console.log(numbersStorage);
+    setLotteryBall([
+      { number: numbersStorage[0] },
+      { number: numbersStorage[1] },
+      { number: numbersStorage[2] },
+      { number: numbersStorage[3] },
+      { number: numbersStorage[4] },
+    ]);
+  };
 
   return (
     <div className="lottery">
       <div className="balls">
-        <LotteryBall number={5}></LotteryBall>
-        <LotteryBall number={11}></LotteryBall>
-        <LotteryBall number={16}></LotteryBall>
-        <LotteryBall number={23}></LotteryBall>
-        <LotteryBall number={32}></LotteryBall>
+        <LotteryBall number={lotteryBall[0].number}></LotteryBall>
+        <LotteryBall number={lotteryBall[1].number}></LotteryBall>
+        <LotteryBall number={lotteryBall[2].number}></LotteryBall>
+        <LotteryBall number={lotteryBall[3].number}></LotteryBall>
+        <LotteryBall number={lotteryBall[4].number}></LotteryBall>
       </div>
       <button onClick={generateNumbers}>Try!</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
